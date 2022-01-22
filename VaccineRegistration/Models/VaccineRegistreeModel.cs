eabc6b85b3a1f80512283a67c5ba50892eefbfe0
dannniell <danielalferian9@gmail.com>
2022-01-22 23:33:21 +0700

    update error message

--- VaccineRegistration/Models/VaccineRegistreeModel.cs
+++ VaccineRegistration/Models/VaccineRegistreeModel.cs
@@ -9,32 +9,32 @@ namespace VaccineRegistration.Models
         [Key]
         public int PatientId  { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Patient Name is is required.")]
         public string PatientName { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Place of Birth Name is is required.")]
         public string PoB { get; set; }
 
         [DataType(DataType.Date)]
         [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd/mm/yyyy}")]
         public DateTime DoB { get; set; }
 
-        [Required]
-        [MinLength(16)]
-        [MaxLength(16)]
+        [Required(ErrorMessage = "NIK Must Be Numeric and 16-Digit")]
+        [MinLength(16, ErrorMessage = "NIK Must Be Numeric and 16-Digit")]
+        [MaxLength(16, ErrorMessage = "NIK Must Be Numeric and 16-Digit")]
         public string NIK { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Address Name is is required")]
         public string Address { get; set; }
 
         public string Province { get; set; }
 
         public string City { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Address Name is is required")]
         public string VaccineType { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Address Name is is required")]
         public string VaccineDose { get; set; }
 
 
