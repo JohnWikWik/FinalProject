111f52076ffb115bcad0d0c8a77b250669dadccb
dannniell <danielalferian9@gmail.com>
2022-01-22 20:20:40 +0700

    validation (belum lengkap)

--- VaccineRegistration/Models/VaccineRegistreeModel.cs
+++ VaccineRegistration/Models/VaccineRegistreeModel.cs
@@ -8,8 +8,13 @@ namespace VaccineRegistration.Models
     {   
         [Key]
         public int PatientId  { get; set; }
+
         [Required]
+        [MinLength(1)]
         public string PatientName { get; set; }
+
+        [Required]
+        [MinLength(1)]
         public string PoB { get; set; }
 
         [DataType(DataType.Date)]
@@ -18,19 +23,26 @@ namespace VaccineRegistration.Models
 
         public string NIK { get; set; }
 
+        [Required]
+        [MinLength(1)]
         public string Address { get; set; }
 
         public string Province { get; set; }
 
         public string City { get; set; }
 
+        [Required]
+        [MinLength(1)]
         public string VaccineType { get; set; }
 
+        [Required]
+        [MinLength(1)]
         public string VaccineDose { get; set; }
 
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime VaccineDate { get; set; }
-
-
 
     }
 }
